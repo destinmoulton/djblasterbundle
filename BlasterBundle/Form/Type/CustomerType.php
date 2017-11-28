c0763fbcfbb66551a9f27218243ce3f72d8113f8
Destin Moulton <destin.moulton@gmail.com>
2017-11-27 18:09:51 -0700

    Replaced string types.

--- BlasterBundle/Form/Type/CustomerType.php
+++ BlasterBundle/Form/Type/CustomerType.php
@@ -5,11 +5,12 @@ namespace DJBlaster\BlasterBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CustomerType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder -> add('name', 'text') -> add('info', 'textarea');
-
+        $builder -> add('name', TextType::class) -> add('info', TextareaType::class);
     }
 
     public function getName() {
