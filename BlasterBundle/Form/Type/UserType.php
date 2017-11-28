0358a9a7702d766801e76460ae074ba4bf542cf6
Destin Moulton <destin.moulton@gmail.com>
2017-11-27 18:14:44 -0700

    Replace string submit with SubmitType.

--- BlasterBundle/Form/Type/UserType.php
+++ BlasterBundle/Form/Type/UserType.php
@@ -5,10 +5,11 @@ namespace DJBlaster\BlasterBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder -> add('name') -> add('email') -> add('save', 'submit');
+        $builder -> add('name') -> add('email') -> add('save', SubmitType::class);
 
     }
 
