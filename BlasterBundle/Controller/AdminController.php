a9e831904048138f2c4695add9ba9a81a2dd9de1
Destin Moulton <destin.moulton@gmail.com>
2017-12-11 14:31:33 -0700

    Added sponsorship reads to the dashboard.

--- BlasterBundle/Controller/AdminController.php
+++ BlasterBundle/Controller/AdminController.php
@@ -9,11 +9,16 @@ use DJBlaster\BlasterBundle\Entity\DJReadPSA;
 
 class AdminController extends Controller {
     public function indexAction() {
+        $sponsorship_reads = $this->getDoctrine()
+            ->getRepository('DJBlasterBundle:DJReadShowSponsorship')
+            ->getRecentReads(20);
+
         $psa_reads = $this->getDoctrine()
             ->getRepository('DJBlasterBundle:DJReadPSA')
             ->getRecentReads(20);
 
         return $this->render('DJBlasterBundle:Admin:admin_index.html.twig', array(
+            'sponsorship_reads'=>$sponsorship_reads,
             'psa_reads'=>$psa_reads
         ));
     }
