07ee75f3cd0202bf021d18bd403e140bfd51057f
Destin Moulton <destin.moulton@gmail.com>
2017-12-11 13:34:35 -0700

    Added query to get the recent DJ reads with associated customer and campaign information.

--- BlasterBundle/Entity/DJReadPSARepository.php
+++ BlasterBundle/Entity/DJReadPSARepository.php
@@ -16,4 +16,20 @@ class DJReadPSARepository extends EntityRepository
     public function findAllForPSA(AdPSA $psa) {
         return $this->findBy(array('psa' => $psa), array('time_read' => 'ASC'));
     }
+
+    public function getRecentReads($numToGet) {
+        $fields = array('r.dj_initials, r.time_read, p.ad_name, p.ad_content, u.name as customer_name');
+        $qb = $this->createQueryBuilder('r');
+        $query = $qb->select($fields)
+            ->innerJoin('DJBlasterBundle:AdPSA', 'p', 'WITH', 'p.psa_id = r.psa')
+            ->innerJoin('DJBlasterBundle:CustomerCampaign', 'c', 'WITH', 'c.campaign_id = p.campaign')
+            ->innerJoin('DJBlasterBundle:Customer', 'u', 'WITH', 'u.id = p.customer')
+            ->orderBy('r.time_read','DESC')
+            ->setMaxResults($numToGet)
+            ->getQuery();        
+
+        $result =  $query->getResult();
+        //echo $query->getSql();var_dump($query->getParameters());die;
+        return $result;
+    }
 }
