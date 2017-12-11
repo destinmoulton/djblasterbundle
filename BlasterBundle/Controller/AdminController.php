189795d57301b8fd0e74302f0ceb855c40891b2d
Destin Moulton <destin.moulton@gmail.com>
2017-12-11 14:10:02 -0700

    Added psa read repository aquisition.

--- BlasterBundle/Controller/AdminController.php
+++ BlasterBundle/Controller/AdminController.php
@@ -5,9 +5,17 @@ namespace DJBlaster\BlasterBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use DJBlaster\BlasterBundle\Entity\DJReadPSA;
+
 class AdminController extends Controller {
     public function indexAction() {
-        return $this->render('DJBlasterBundle:Admin:admin_index.html.twig');
+        $psa_reads = $this->getDoctrine()
+            ->getRepository('DJBlasterBundle:DJReadPSA')
+            ->getRecentReads(20);
+
+        return $this->render('DJBlasterBundle:Admin:admin_index.html.twig', array(
+            'psa_reads'=>$psa_reads
+        ));
     }
 
 }
