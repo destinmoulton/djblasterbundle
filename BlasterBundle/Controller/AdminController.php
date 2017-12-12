770eea58426aa4f6e8be225ee5cec00e3a71dacf
Destin Moulton <destin.moulton@gmail.com>
2017-12-11 18:13:26 -0700

    Added method to collate ad campaign information.

--- BlasterBundle/Controller/AdminController.php
+++ BlasterBundle/Controller/AdminController.php
@@ -9,23 +9,56 @@ use DJBlaster\BlasterBundle\Entity\DJReadPSA;
 
 class AdminController extends Controller {
     public function indexAction() {
+        $active_campaigns = $this->getDoctrine()
+            ->getRepository('DJBlasterBundle:CustomerCampaign')
+            ->getActiveCampaigns();
+
+        $active_campaign_ads = $this->_collateCampaignAds($active_campaigns);
+
         $sponsorship_reads = $this->getDoctrine()
             ->getRepository('DJBlasterBundle:DJReadShowSponsorship')
-            ->getRecentReads(20);
+            ->getRecentReads(40);
 
         $event_reads = $this->getDoctrine()
             ->getRepository('DJBlasterBundle:DJReadEvent')
-            ->getRecentReads(20);
+            ->getRecentReads(40);
 
         $psa_reads = $this->getDoctrine()
             ->getRepository('DJBlasterBundle:DJReadPSA')
-            ->getRecentReads(20);
+            ->getRecentReads(40);
 
         return $this->render('DJBlasterBundle:Admin:admin_index.html.twig', array(
+            'active_campaigns'=>$active_campaigns,
+            'active_campaign_ads'=>$active_campaign_ads,
             'sponsorship_reads'=>$sponsorship_reads,
             'event_reads'=>$event_reads,
             'psa_reads'=>$psa_reads
         ));
     }
 
+    // Get all the Show Sponsorships, Events/Promotions, and PSAs for a campaign
+    private function _collateCampaignAds($campaigns){
+        $collate = array();
+        foreach($campaigns as $camp){
+            $campaignObj = $this->getDoctrine()
+                            ->getRepository('DJBlasterBundle:CustomerCampaign')
+                            ->find($camp['campaign_id']);
+            //echo($camp['campaign_id']);die();
+            $collate[$camp['campaign_id']] = array();
+            $collate[$camp['campaign_id']]['sponsorships'] = $this->getDoctrine()
+                ->getRepository('DJBlasterBundle:AdShowSponsorship')
+                ->findAllOrderedByNameForCampaignId($campaignObj);
+
+            $collate[$camp['campaign_id']]['events'] = $this->getDoctrine()
+                ->getRepository('DJBlasterBundle:AdEvent')
+                ->findAllOrderedByNameForCampaignId($campaignObj);
+            
+            $collate[$camp['campaign_id']]['psas'] = $this->getDoctrine()
+                ->getRepository('DJBlasterBundle:AdPSA')
+                ->findAllOrderedByNameForCampaignId($campaignObj);
+
+        }
+
+        return $collate;
+    }
 }
